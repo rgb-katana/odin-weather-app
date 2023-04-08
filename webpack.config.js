69209113bb464fef87f9f62dee07bd04300f11b3
rgb-katana <komarova1610@yandex.ru>
2023-04-08 11:07:40 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+module.exports = {
+  mode: mode,
+
+  plugins: [new MiniCssExtractPlugin()],
+
+  entry: './src/js/controller.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    // clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_mudules/,
+        use: {
+          // Without additional settings this will reference .babelrc
+          loader: 'babel-loader',
+        },
+      },
+
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+          'postcss-loader',
+        ],
+      },
+
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  devtool: 'source-map',
+
+  devServer: {
+    static: './dist',
+  },
+};
